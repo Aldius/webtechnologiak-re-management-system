a1b6e5dff57e936ef066418e854990c98d0b5741
Horváth Ádám <adam.horvath@loxon.eu>
2020-02-29 14:35:45 +0100

    Added Maven parent pom and example project

---
+++ real-estate-data-store/src/main/java/hu/elte/webtechnologiak/realestate/RealEstateDatastoreApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.webtechnologiak.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateDatastoreApplication {
+
+	public static void main( String[] args ) {
+		SpringApplication.run(RealEstateDatastoreApplication.class, args);
+	}
+
+}
