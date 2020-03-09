64b7090347e57e851421bc427f691a23b84fb3c5
Vadász Péter <pvadasz@systemathx.hu>
2020-03-09 22:30:58 +0100

    Data-Store-5: Real estate data handling and CRUD operations.

--- real-estate-data-store/src/main/java/webtech/realestatemanagement/realestatedatastore/model/BaseEntity.java
+++ real-estate-data-store/src/main/java/webtech/realestatemanagement/realestatedatastore/model/BaseEntity.java
@@ -1,12 +1,18 @@
 package webtech.realestatemanagement.realestatedatastore.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @MappedSuperclass
+@Data
 public class BaseEntity {
 
+    public static final int ACTIVE_ENTITY_STATUS = 0;
+    public static final int INACTIVE_ENTITY_STATUS = 1;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected long id;
 
     @Version
