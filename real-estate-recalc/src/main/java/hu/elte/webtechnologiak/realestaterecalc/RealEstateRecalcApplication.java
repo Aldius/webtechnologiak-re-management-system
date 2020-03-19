00ef20aa141e5e8ab0635c013e66656d3bfd3021
Márk Vecsernyés <kurtaazz@gmail.com>
2020-03-19 14:37:44 +0100

    Added generated folder

---
+++ real-estate-recalc/src/main/java/hu/elte/webtechnologiak/realestaterecalc/RealEstateRecalcApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.webtechnologiak.realestaterecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateRecalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateRecalcApplication.class, args);
+	}
+
+}
