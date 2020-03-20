bc95b61d95ae09b764474250b8cbabf0341e0b71
Márk Vecsernyés <kurtaazz@gmail.com>
2020-03-20 18:24:47 +0100

    cleanup2

--- real-estate-recalc/src/main/java/hu/elte/webtechnologiak/realestaterecalc/RealEstateRecalcApplication.java
+++ real-estate-recalc/src/main/java/hu/elte/webtechnologiak/realestaterecalc/RealEstateRecalcApplication.java
@@ -2,12 +2,14 @@ package hu.elte.webtechnologiak.realestaterecalc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RealEstateRecalcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealEstateRecalcApplication.class, args);
 	}
-
 }
