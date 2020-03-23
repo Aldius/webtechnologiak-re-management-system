453681e00542159699d7f203f6ea3dbadfbd5a2e
Horváth Ádám <aldius@inf.elte.hu>
2020-03-23 19:35:31 +0100

    External API module refactor

--- real-estate-external-api-module/src/main/java/webtech/externalapimodule/service/ForecastRetrieverImpl.java
+++ real-estate-external-api-module/src/main/java/webtech/externalapimodule/service/ForecastRetrieverImpl.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
