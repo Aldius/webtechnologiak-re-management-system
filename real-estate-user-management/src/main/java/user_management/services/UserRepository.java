71fe1a5f5aaf4b4029baa0f66f30a3d90284a614
leaderhun <norbika1997@gmail.com>
2020-03-30 21:24:17 +0200

    registration and authentication with jwt token

--- real-estate-user-management/src/main/java/user_management/services/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package user_management.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import user_management.models.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- real-estate-user-management/src/main/java/user_management/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package user_management.services;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class UserService {
-    @Autowired
-    private UserRepository userRepository;
-}
